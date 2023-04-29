5959545e301930e83537450c4eedc7fb43b5e117
ManideepBangaru <bmd994@gmail.com>
2023-04-29 16:35:22 +0530

    updated the skin and look

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navBarLinks = document.getElementsByClassName('nav-bar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+    navBarLinks.classList.toggle('active')
+}
+)
